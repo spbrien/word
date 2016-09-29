0db4531c1f725365ee68e53148379724919b1f0e
Steven Brien <sbrien@hlkagency.com>
2016-09-29 16:14:09 -0500

    Added some basic tasks to fabfile for testing

--- word/templates/fabfile.py
+++ word/templates/fabfile.py
@@ -1,6 +1,31 @@
-{% raw %}from fabric.api import *
+from fabric.api import *
 from fabtools.vagrant import vagrant
 
+# Set up constants
+PROJECT_NAME = "{{project_name}}"
+WORDPRESS_DIR = "/var/www/{{project_name}}"
+
+
 @task
-def uname():
-    run('uname -a'){% endraw %}
+def backup():
+    with cd(WORDPRESS_DIR):
+        run('wp db export --add-drop-table')
+
+
+@task
+def update():
+    with cd(WORDPRESS_DIR):
+        run('wp core update')
+        run('wp core update-db')
+
+
+@task
+def regenerate_media():
+    with cd(WORDPRESS_DIR):
+        run('wp media regenerate --yes')
+
+
+@task
+def replace(old, new):
+    with cd(WORDPRESS_DIR):
+        run('wp search-replace "%s" "%s"' % (old, new))
